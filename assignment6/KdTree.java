71470a607d32c6f522feb709ff53592216a24c22
pvd232 <50126818+pvd232@users.noreply.github.com>
2023-11-15 15:56:22 -0500

    updated folder name, completed course

--- assignment6/KdTree.java
+++ assignment6/KdTree.java
@@ -295,7 +295,6 @@ public class KdTree {
                 if (tmpDist < dist) {
                     dist = tmpDist;
                     bruteTestNearestPoint = p.val;
-
                 }
             }
         }
